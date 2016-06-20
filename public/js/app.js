66b9d0b6ad78d88222518ce8bb77991da3dcd11f
ASHISH AGGARWAL <ashishjyc@gmail.com>
2016-06-20 19:55:49 -0400

    added the seconds counter

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log('My Javascript is working.....Yeahh...');
+$(document).ready(function(){
+  var start=$('#start');
+  var minutes=$('#minutes');
+  var seconds=$('#seconds');
+
+  start.on('click', startCountdown);
+
+  function startCountdown(){
+    setInterval(function(){
+      var secondsVal=+seconds.text(); //the plus makes it behave like a number
+      if(secondsVal===0){
+        seconds.text(59);
+      }else {
+        seconds.text(secondsVal - 1);
+      }
+
+    }, 1000);
+  }
+});
