c61aa4f4fb85d987bf7c0fea259f6ff23a853280
ASHISH AGGARWAL <ashishjyc@gmail.com>
2016-06-20 20:22:08 -0400

    added the minute counter

--- public/js/app.js
+++ public/js/app.js
@@ -8,10 +8,31 @@ $(document).ready(function(){
   function startCountdown(){
     setInterval(function(){
       var secondsVal=+seconds.text(); //the plus makes it behave like a number
+      var minutesVal=+minutes.text();
       if(secondsVal===0){
         seconds.text(59);
       }else {
-        seconds.text(secondsVal - 1);
+        if(secondsVal<11){
+          seconds.text("0" + (secondsVal -1))
+        }
+        else{
+          seconds.text(secondsVal - 1);
+        }
+
+      }
+
+      if(secondsVal===0){
+        if(minutesVal<11){
+          minutes.text("0" + (minutesVal - 1))
+        }
+        else{
+          minutes.text(minutesVal-1);
+        }
+      }
+
+      if(minutesVal==='0' && secondsVal==='0')
+      {
+        break;
       }
 
     }, 1000);
